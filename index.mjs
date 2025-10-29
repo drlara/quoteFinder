d57084fd66440c2f27897abc8b93bb3359f75345
mist-miguel-lara <123275349+mist-miguel-lara@users.noreply.github.com>
2025-10-28 22:32:12 -0700

    quoteFinder - search by keyword

---
+++ index.mjs
@@ -0,0 +1,61 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "w9c7lwn8um1o99yj",
+    password: "u3rw8lbcasz2h307",
+    database: "pyn5h5u7iu857dd2",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+//routes
+app.get('/', (req, res) => {
+   res.render('home.ejs')
+});
+
+app.get('/searchByKeyword', async(req, res) => {
+   let keyword = req.query.keyword;
+   let sql = `SELECT authorId, firstName, lastName, quote
+              FROM authors
+              NATURAL JOIN quotes
+              WHERE quote LIKE ?`;
+    let sqlParams = [`%${keyword}%`];
+    const [rows] = await pool.query(sql, sqlParams);
+    console.log(rows);
+   res.render('results.ejs', {rows})
+});
+
+//local API to get all info for a specific author
+app.get('/api/authors/:authorId', async(req, res) => {
+   let authorId = req.params.authorId;
+   let sql = `SELECT *
+              FROM authors
+              WHERE authorId = ?`;
+  const [rows] = await pool.query(sql, [authorId]);          
+  res.send(rows);
+});
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
